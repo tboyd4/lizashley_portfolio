65583655005886fd2476cad62ef1363ae7903473
Tyler Boyd <tylerb@theboydprojects.com>
2020-08-23 14:47:24 -0500

    demos play on button click

--- components/Demo.vue
+++ components/Demo.vue
@@ -3,10 +3,22 @@
     <v-row justify="center" align="center">
       <v-col justify="center" align="center" cols="12" md="6">
         <v-row>
-          <v-btn color="main main--text" depressed outlined class="mx-1">
+          <v-btn
+            color="main main--text"
+            @click="narrPlay"
+            depressed
+            outlined
+            class="mx-1"
+          >
             Click for Narrative Demo
           </v-btn>
-          <v-btn color="main main--text" depressed outlined class="mx-1">
+          <v-btn
+            color="main main--text"
+            @click="commPlay"
+            depressed
+            outlined
+            class="mx-1"
+          >
             Click for Commercial Demo
           </v-btn>
         </v-row>
@@ -19,7 +31,23 @@
 </template>
 
 <script>
-export default {}
+import {Howl, Howler} from 'howler';
+const narrSound = new Howl({
+  src: ['/narrdemo.mp3'],
+})
+const commSound = new Howl({
+  src: ['/commdemo.mp3'],
+})
+export default {
+  methods: {
+    narrPlay() {
+      narrSound.play()
+    },
+    commPlay() {
+      commSound.play()
+    },
+  },
+}
 </script>
 
 <style scoped></style>
